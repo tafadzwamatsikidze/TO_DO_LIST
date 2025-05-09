53512127bc3497451fca0f87365e182a1012a73e
tafadzwamike <171960080+tafadzwamatsikidze@users.noreply.github.com>
2025-05-09 13:15:52 +0200

    my first intial commit

---
+++ includes/config.php
@@ -0,0 +1,16 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+$conn = new mysqli(
+    $_ENV['DB_HOST'],
+    $_ENV['DB_USER'],
+    $_ENV['DB_PASS'],
+    $_ENV['DB_NAME']
+);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
